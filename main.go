8357d7a084b35380f21c89c20db730f6e5c2e965
saf1u <a_safwa@live.cocnordia.ca>
2021-09-21 22:00:52 -0400

    finished rough implemetation

--- main.go
+++ main.go
@@ -10,25 +10,6 @@ import (
 	"strings"
 )
 
-type response struct {
-	statusLine string //contains protocolMethod SP respcode SP phrase associated with code
-	headers    map[string]string
-	//resp Headers
-	// Location
-	// Server
-	// WWW-Authenthicate
-
-	//entity headers
-	//                     Allow                    ; Section 10.1
-	//                   | Content-Encoding         ; Section 10.3
-	//                   | Content-Length           ; Section 10.4
-	//                   | Content-Type             ; Section 10.5
-	//                   | Expires                  ; Section 10.7
-	//                   | Last-Modified            ; Section 10.10
-	//                   | extension-header
-
-}
-
 func main() {
 	rd := bufio.NewScanner(strings.NewReader("httpc post -h Content-Type:application/json -d '{\"Assignment\": 1}' 'http://httpbin.org/post' "))
 	//"httpc post -v -h User-Agent:Mozilla/5.0 -d 'goo'  'http://httpbin.org/get?course=networking&assignment=1' "))
