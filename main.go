9d0a979473301c8f157526cfd3e9f4c0466c56dc
saf1u <a_safwa@live.cocnordia.ca>
2021-09-22 12:03:43 -0400

    made it smooth

--- main.go
+++ main.go
@@ -11,10 +11,69 @@ import (
 	"strings"
 )
 
+var (
+	get = `httpc help get
+	usage: httpc get [-v] [-h key:value] URL
+	Get executes a HTTP GET request for a given URL.
+	 -v Prints the detail of the response such as protocol, status,
+	and headers.
+	 -h key:value Associates headers to HTTP Request with the format
+	'key:value'.`
+
+	post = `usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL
+	Post executes a HTTP POST request for a given URL with inline data or from
+	file.
+	 -v Prints the detail of the response such as protocol, status,
+	and headers.
+	 -h key:value Associates headers to HTTP Request with the format
+	'key:value'.
+	 -d string Associates an inline data to the body HTTP POST request.
+	 -f file Associates the content of a file to the body HTTP POST
+	request.`
+	help = `httpc help
+    httpc is a curl-like application but supports HTTP protocol only.
+    Usage:
+    httpc command [arguments]
+    The commands are:
+ 	get executes a HTTP GET request and prints the response.
+ 	post executes a HTTP POST request and prints the response.
+ 	help prints this screen.
+	Use "httpc help [command]" for more information about a command.
+	`
+)
+
+//"httpc post -v -h User-Agent:Mozilla/5.0 -d 'goo'  'http://httpbin.org/get?course=networking&assignment=1' "))
+//rd := bufio.NewScanner(strings.NewReader("httpc post -h Content-Type:application/json -d '{"assignment": 1}' 'http://httpbin.org/post' "))
+
 func main() {
-	//rd := bufio.NewScanner(strings.NewReader("httpc post -h Content-Type:application/json -d '{\"assignment\": 1}' 'http://httpbin.org/post' "))
+	for {
+		httpc()
+	}
+
+}
+
+func httpc() {
 	reader := bufio.NewReader(os.Stdin)
 	query, err := reader.ReadString('\n')
+	check := true
+	for check {
+		list := strings.TrimSpace(query)
+		switch {
+		case list == "httpc help":
+			fmt.Println(help)
+			query, _ = reader.ReadString('\n')
+		case list == "httpc help get":
+			fmt.Println(get)
+			query, _ = reader.ReadString('\n')
+		case list == "httpc help post":
+			fmt.Println(post)
+			query, _ = reader.ReadString('\n')
+		default:
+			check = false
+
+		}
+
+	}
 	query = query[0 : len(query)-1]
 	if err != nil {
 		log.Println(err)
@@ -22,7 +81,6 @@ func main() {
 	}
 	query = query + " "
 	rd := bufio.NewScanner(strings.NewReader(query))
-	//"httpc post -v -h User-Agent:Mozilla/5.0 -d 'goo'  'http://httpbin.org/get?course=networking&assignment=1' "))
 	req, err := parser.Parse(rd)
 	if err != nil {
 		log.Println(err)
@@ -50,5 +108,4 @@ func main() {
 		fmt.Println(string(buf))
 
 	}
-
 }
