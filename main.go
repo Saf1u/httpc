4138aa61c6848f2895506ba0cb23d147d6a4298e
saf1u <a_safwa@live.cocnordia.ca>
2021-09-21 22:24:42 -0400

    finished rough implemetation

--- main.go
+++ main.go
@@ -7,11 +7,21 @@ import (
 	"net"
 	"net445/connection"
 	"net445/parser"
+	"os"
 	"strings"
 )
 
 func main() {
-	rd := bufio.NewScanner(strings.NewReader("httpc post -h Content-Type:application/json -d '{\"Assignment\": 1}' 'http://httpbin.org/post' "))
+	//rd := bufio.NewScanner(strings.NewReader("httpc post -h Content-Type:application/json -d '{\"assignment\": 1}' 'http://httpbin.org/post' "))
+	reader := bufio.NewReader(os.Stdin)
+	query, err := reader.ReadString('\n')
+	query = query[0 : len(query)-1]
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	query = query + " "
+	rd := bufio.NewScanner(strings.NewReader(query))
 	//"httpc post -v -h User-Agent:Mozilla/5.0 -d 'goo'  'http://httpbin.org/get?course=networking&assignment=1' "))
 	req, err := parser.Parse(rd)
 	if err != nil {
